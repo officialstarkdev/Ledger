30e2cf1b0dc5a1353281c5e437f4c10e60192fa9
thetonyst14-droid <thetonyst14@gmail.com>
2026-03-14 12:07:12 +0500

    Ledger

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 import app from "../server/server.js";
+import connectDB from "../server/config/db.js";
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
+  await connectDB();
   return app(req, res);
 }
--- server/server.js
+++ server/server.js
@@ -62,11 +62,13 @@ app.use(errorHandler);
 // Start server
 const PORT = process.env.PORT || 5000;
 
-connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-        console.log(`App available at http://localhost:${PORT}`);
+if (process.env.NODE_ENV !== 'production' || process.env.VERCEL !== '1') {
+    connectDB().then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+            console.log(`App available at http://localhost:${PORT}`);
+        });
     });
-});
+}
 
 export default app;
